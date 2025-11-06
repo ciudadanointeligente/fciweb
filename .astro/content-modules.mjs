b7fde819296138f8863b0c1af1c2ebe5e28c6e76
pedregalux <felipe.perry@vivaldi.net>
2025-11-05 22:04:46 -0300

    nos

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
 ["src/content/posts/articulo-numero-23.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Farticulo-numero-23.mdoc&astroContentModuleFlag=true")],
-["src/content/posts/first-post.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffirst-post.mdoc&astroContentModuleFlag=true")],
-["src/content/posts/texto-tentativo.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftexto-tentativo.mdoc&astroContentModuleFlag=true")],
 ["src/content/posts/post-dos.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fpost-dos.mdoc&astroContentModuleFlag=true")],
-["src/content/posts/post-tres.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fpost-tres.mdoc&astroContentModuleFlag=true")]]);
+["src/content/posts/texto-tentativo.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ftexto-tentativo.mdoc&astroContentModuleFlag=true")],
+["src/content/posts/post-tres.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fpost-tres.mdoc&astroContentModuleFlag=true")],
+["src/content/posts/first-post.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffirst-post.mdoc&astroContentModuleFlag=true")]]);
 		
