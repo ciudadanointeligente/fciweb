09ba80e72beba8f7fe2bab3e4f9b11bf6a323c83
pedregalux <felipe.perry@vivaldi.net>
2025-11-28 10:54:49 -0300

    colmena7

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,11 +2,7 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'github',
-    repo: {
-      owner: ciudadaniainteligente,
-      name: fciweb,
-    },
+    kind: 'local',
   },
   collections: {
     posts: collection({
