b3dc735811e5fa908108f758bf6820c4bbaf8a77
pedregalux <felipe.perry@vivaldi.net>
2026-05-08 14:27:04 -0400

    fix react

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import netlify from "@astrojs/netlify";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   site: "https://fciweb.netlify.app/",
   output: "server",
-  integrations: [markdoc(), keystatic(), alpinejs(), icon()],
+  integrations: [markdoc(), keystatic(), alpinejs(), icon(), react()],
 
   vite: {
     plugins: [tailwindcss()],
